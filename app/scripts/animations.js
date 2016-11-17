cebce8e14dce8bf864f44fcc0a52659aef9da55b
Tim Park <timothsp@uci.edu>
2016-11-17 13:21:15 -0800

    Minor text fixes

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -4,7 +4,22 @@ $(function(){
     	typeSpeed: 10,
     	startDelay: 500,
     	callback: function() {
-    		$(".typed-cursor").delay(1220).fadeOut(100);
+    		$(".typed-cursor").css('display','none');
+    		setTimeout(invokeFade,400);
     	}
 	});
 });
+
+function invokeFade() {
+	fadeIn(0, $('a'), 400);
+}
+
+function fadeIn(i, elements, duration, callback){
+    if(i >= elements.length)
+        $.isFunction(callback) && callback();
+    else
+        elements.eq(i).animate({color: 'rgba(0,0,0,1)'},duration, function(){
+        	elements.eq(i).parent().removeClass('disabled');
+        	fadeIn(i+1, elements, duration, callback);
+        }).css("display","inline");        
+}
